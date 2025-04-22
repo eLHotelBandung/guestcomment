2b51836680d845acaca0dc91f1290fbb47b2e229
éLhotelBandung <njoy.divisions@gmail.com>
2025-04-22 14:42:14 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+var calificar = document.getElementById("calificar");
+function ocultar(){
+if (calificar.style.display == "none"){
+
+  calificar.style.display = "block";
+}else{
+calificar.style.display = "none";
+}
+}
